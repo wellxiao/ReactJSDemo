323c251ab8499a96a44bd67ec97aeb7da4caeba4
wei.cao <wei.cao@lycheegame.com>
2018-03-21 17:43:15 +0800

    双向数据绑定

--- src/component/message/index.js
+++ src/component/message/index.js
@@ -4,11 +4,16 @@ class Message extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "wellxiao"
+      name: "wellxiao",
+      mess: "上山打老虎"
     };
   }
   handleClick = () => {
     console.log(this.state.name);
+  };
+  handelChange = event => {
+    console.log(event.target);
+    this.setState({ mess: event.target.value });
   };
   render() {
     console.log(this);
@@ -16,6 +21,12 @@ class Message extends Component {
       <div>
         <h3>Message get name is : {this.state.name}</h3>
         <button onClick={this.handleClick}> 我的名字</button>
+        <input
+          type="text"
+          onChange={this.handelChange}
+          value={this.state.mess}
+        />
+        <b>{this.state.mess}</b>
       </div>
     );
   }
